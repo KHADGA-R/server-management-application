627d399178e8bc2af68ea828411286ef94da6896
KHADGA-R <parajuli.kr@gmail.com>
2022-05-02 18:36:07 -0400

    configuration setup with database

--- BackEnd/src/main/java/com/example/server/ServerApplication.java
+++ BackEnd/src/main/java/com/example/server/ServerApplication.java
@@ -1,7 +1,12 @@
 package com.example.server;
 
+import com.example.server.enumeration.Status;
+import com.example.server.models.Server;
+import com.example.server.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -9,5 +14,16 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.55", "Fedora Linux", "16 GB", "Mail Server", "http://localhost:8080/server/image/server2.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.170", "MS 2010", "32 GB", "Web Server", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.190", "Red Hat Enterprise Linux", "64 GB", "Dell Tower", "http://localhost:8080/server/image/server4.png", Status.SERVER_UP));
+		};
+	}
+
 
 }
